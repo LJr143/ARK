a8fba5a5ac87f618a84083a59a267205849db648
JohnnySons <lorjohn143@gmail.com>
2025-06-14 13:09:54 +0800

    bulk commit v.5 - changes - comments

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <link rel="shortcut icon" href="{{ asset('storage/logo/ark-logo-bg-none.ico') }}" type="image/x-icon">
+    <link rel="icon" type="image/x-icon" href="{{ asset('storage/logo/ark-logo-bg-none.ico') }}">
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
