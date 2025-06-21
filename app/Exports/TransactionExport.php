e5a773489ae20ecdf07c266b3b2db2eb660aab40
JohnnySons <lorjohn143@gmail.com>
2025-06-21 20:42:41 +0800

    bulk commit v.17 - changes -

--- app/Exports/TransactionExport.php
+++ app/Exports/TransactionExport.php
@@ -4,14 +4,39 @@ namespace App\Exports;
 
 use App\Models\Transaction;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class TransactionExport implements FromCollection
+class TransactionExport implements FromCollection, WithHeadings
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
+     * Return the collection of data to export.
+     */
     public function collection()
     {
-        return Transaction::all();
+        return Transaction::all([
+            'id',
+            'transaction_reference',
+            'payment_method',
+            'amount',
+            'status',
+            'payer_email',
+            'completed_at'
+        ]);
+    }
+
+    /**
+     * Return the headings for the spreadsheet.
+     */
+    public function headings(): array
+    {
+        return [
+            'ID',
+            'Transaction Reference',
+            'Payment Method',
+            'Amount',
+            'Status',
+            'Payer Email',
+            'Completed At'
+        ];
     }
 }
