f435a755d5e19f104b95cad193b7bb1388377ef3
JohnnySons <lorjohn143@gmail.com>
2025-06-22 17:56:36 +0800

    bulk commit v.19 - changes -

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use App\Traits\HandlesProfilePhotoUploads;
 use Database\Factories\UserFactory;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
@@ -21,8 +20,7 @@ class User extends Authenticatable
 
     /** @use HasFactory<UserFactory> */
     use HasFactory;
-//    use HasProfilePhoto;
-    use HandlesProfilePhotoUploads;
+    use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
 
